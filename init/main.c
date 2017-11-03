a8b33aa773d39b810749ae1dd1bd3eab26ae6a93
Tal Shorer <tal.shorer@gmail.com>
2017-11-03 17:27:49 +0200

    main: kernel_start: move housekeeping_init() before workqueue_init_early()
    This is needed in order to allow the unbound workqueue to take
    housekeeping cpus into accounty
    
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- init/main.c
+++ init/main.c
@@ -621,6 +621,12 @@ asmlinkage __visible void __init start_kernel(void)
 	radix_tree_init();
 
 	/*
+	 * Set up housekeeping before setting up workqueues to allow the unbound
+	 * workqueue to take non-housekeeping into account.
+	 */
+	housekeeping_init();
+
+	/*
 	 * Allow workqueue creation and work item queueing/cancelling
 	 * early.  Work item execution depends on kthreads and starts after
 	 * workqueue_init().
