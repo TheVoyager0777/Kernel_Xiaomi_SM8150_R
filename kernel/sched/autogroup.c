abe71c7ab3eb79519871437e3a17eafbbea6b06c
Sergey Senozhatsky <sergey.senozhatsky.work@gmail.com>
2017-12-08 17:24:22 +0900

    sched/autogroup: move sched.h include
    Move local "sched.h" include to the bottom. sched.h defines
    several macros that are getting redefined in ARCH-specific
    code, for instance, finish_arch_post_lock_switch() and
    prepare_arch_switch(), so we need ARCH-specific definitions
    to come in first.
    
    Link: http://lkml.kernel.org/r/20171208082422.5021-1-sergey.senozhatsky@gmail.com
    To: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: LKML <linux-kernel@vger.kernel.org>
    Cc: linux-pm@vger.kernel.org
    Cc: linux-pci@vger.kernel.org
    Cc: linux-mm@kvack.org
    Suggested-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Petr Mladek <pmladek@suse.com>

--- kernel/sched/autogroup.c
+++ kernel/sched/autogroup.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: GPL-2.0
-#include "sched.h"
-
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/kallsyms.h>
@@ -8,6 +6,8 @@
 #include <linux/security.h>
 #include <linux/export.h>
 #include <linux/nospec.h>
+
+#include "sched.h"
 
 unsigned int __read_mostly sysctl_sched_autogroup_enabled = 1;
 static struct autogroup autogroup_default;
