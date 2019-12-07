38a7b55c37efc54a0ed889d1d53f73581250cada
UtsavBalar1231 <utsavbalar1231@gmail.com>
2019-12-07 14:32:52 +0000

    drivers: input: Add Support for touch events debugging
    Change-Id: I04ebdbf1b0b6fbbbe8d110345ad6cd16cabf4e1b
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -487,15 +487,7 @@ static int dsi_panel_power_on(struct dsi_panel *panel)
 {
 	int rc = 0;
 
-	if(panel->is_tddi_flag) {
-		if(!is_tp_doubleclick_enable()||panel->panel_dead_flag) {
-			rc = dsi_pwr_enable_regulator(&panel->power_info, true);
-			if(panel->panel_dead_flag)
-				panel->panel_dead_flag = false;
-		}
-	} else {
 		rc = dsi_pwr_enable_regulator(&panel->power_info, true);
-	}
 
 	mdelay(12);
 	if (rc) {
@@ -544,15 +536,8 @@ static int dsi_panel_power_off(struct dsi_panel *panel)
 	if (gpio_is_valid(panel->reset_config.disp_en_gpio))
 		gpio_set_value(panel->reset_config.disp_en_gpio, 0);
 
-	if(panel->is_tddi_flag) {
-		if(!is_tp_doubleclick_enable()||panel->panel_dead_flag) {
-			if (gpio_is_valid(panel->reset_config.reset_gpio))
-				gpio_set_value(panel->reset_config.reset_gpio, 0);
-		}
-	} else {
-		if (gpio_is_valid(panel->reset_config.reset_gpio))
-			gpio_set_value(panel->reset_config.reset_gpio, 0);
-	}
+	if (gpio_is_valid(panel->reset_config.reset_gpio))
+		gpio_set_value(panel->reset_config.reset_gpio, 0);
 
 	if (gpio_is_valid(panel->reset_config.lcd_mode_sel_gpio))
 		gpio_set_value(panel->reset_config.lcd_mode_sel_gpio, 0);
@@ -564,17 +549,9 @@ static int dsi_panel_power_off(struct dsi_panel *panel)
 	}
 	mdelay(20);
 
-	if(panel->is_tddi_flag) {
-		if(!is_tp_doubleclick_enable()||panel->panel_dead_flag) {
-			rc = dsi_pwr_enable_regulator(&panel->power_info, false);
-			if (rc)
-				pr_err("[%s] failed to enable vregs, rc=%d\n", panel->name, rc);
-		}
-	} else {
 		rc = dsi_pwr_enable_regulator(&panel->power_info, false);
 		if (rc)
 			pr_err("[%s] failed to enable vregs, rc=%d\n", panel->name, rc);
-	}
 
 	return rc;
 }
