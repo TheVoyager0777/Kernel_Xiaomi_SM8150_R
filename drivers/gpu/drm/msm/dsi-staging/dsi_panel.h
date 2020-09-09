9383224de2e236fa7f8b2f0d63de8bd46e56bf19
Arian <arian.kulmer@web.de>
2020-09-09 19:41:15 +0200

    drm: msm: Add support for backlight remap
    * Logic from phoenix-r-oss
    
    Change-Id: I9182a7b2cac2c647fa7cca479262a1d5eaa47d45

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -141,6 +141,7 @@ struct dsi_backlight_config {
 	bool dcs_type_ss_eb;
 	bool xiaomi_f4_36_flag;
 	bool xiaomi_f4_41_flag;
+	bool bl_remap_flag;
 
 	int en_gpio;
 	bool bl_remap_flag;
--- drivers/gpu/drm/msm/sde/sde_connector.c
+++ drivers/gpu/drm/msm/sde/sde_connector.c
@@ -113,9 +113,13 @@ static int sde_backlight_device_update_status(struct backlight_device *bd)
 	} else
 		bl_lvl = brightness;
 
-		/* map UI brightness into driver backlight level rounding it */
-		bl_lvl = bl_min + DIV_ROUND_CLOSEST((brightness - 1) * bl_range,
-			display->panel->bl_config.brightness_max_level - 1);
+		if (!display->panel->bl_config.bl_remap_flag) {
+			/* map UI brightness into driver backlight level rounding it */
+			bl_lvl = bl_min + DIV_ROUND_CLOSEST((brightness - 1) * bl_range,
+					display->panel->bl_config.brightness_max_level - 1);
+		} else {
+			bl_lvl = brightness;
+		}
 	} else {
 		bl_lvl = 0;
 	}
