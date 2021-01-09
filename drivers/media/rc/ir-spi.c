adfb327de86ae2202ce957a6a7d3c70b9a28b78a
Pranav Vashi <neobuddy89@gmail.com>
2021-01-09 11:38:02 +0530

    media: rc: Import IR driver changes from MI 11
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I1efd5d0aedac7732f4118da83a2adfc2e92bdd76
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/media/rc/ir-spi.c
+++ drivers/media/rc/ir-spi.c
@@ -17,6 +17,8 @@
 #include <linux/regulator/consumer.h>
 #include <linux/spi/spi.h>
 #include <media/rc-core.h>
+#include "media/lirc_dev.h"
+#include <linux/miscdevice.h>
 
 #define IR_SPI_DRIVER_NAME		"ir-spi"
 
