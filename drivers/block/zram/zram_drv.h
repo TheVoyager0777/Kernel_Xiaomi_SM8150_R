2ad9468e137d54834e89aeac8a19f27e28744011
shaowanxing <shaowanxing@xiaomi.com>
2021-03-16 17:46:30 +0800

    [Performance][EXT_MEM] ZRAM TRACKING
    MIUI-1820366
    
    1.add time_list, pages_life,avg_size
    2.add origin_pages_max, wb_pages_max,average_size
    
    Signed-off-by: shaowanxing <shaowanxing@xiaomi.com>
    Change-Id: Ide078564a4e2087a632c532f4583c28b8817550e

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -83,7 +83,7 @@ struct zram_table_entry {
 		unsigned long element;
 	};
 	unsigned long flags;
-#ifdef CONFIG_ZRAM_MEMORY_TRACKING
+#if defined(CONFIG_ZRAM_MEMORY_TRACKING) || defined(CONFIG_MIUI_ZRAM_MEMORY_TRACKING)
 	ktime_t ac_time;
 #endif
 };
@@ -100,6 +100,9 @@ struct zram_stats {
 	atomic64_t huge_pages;		/* no. of huge pages */
    	atomic64_t huge_pages_since;	/* no. of huge pages since zram set up */
 	atomic64_t pages_stored;	/* no. of pages currently stored */
+#ifdef CONFIG_MIUI_ZRAM_MEMORY_TRACKING
+	atomic64_t origin_pages_max;	/* no. of maximum origin pages stored */
+#endif
 	atomic_long_t max_used_pages;	/* no. of maximum pages stored */
 	atomic64_t writestall;		/* no. of write slow paths */
 	atomic64_t dup_data_size;	/*
@@ -112,6 +115,9 @@ struct zram_stats {
 	atomic64_t bd_count;		/* no. of pages in backing device */
 	atomic64_t bd_reads;		/* no. of reads from backing device */
 	atomic64_t bd_writes;		/* no. of writes from backing device */
+#ifdef CONFIG_MIUI_ZRAM_MEMORY_TRACKING
+	atomic64_t wb_pages_max;	/* no. of max pages in backing device */
+#endif
 #endif
 };
 
@@ -119,6 +125,14 @@ struct zram_hash {
 	spinlock_t lock;
 	struct rb_root rb_root;
 };
+#ifdef CONFIG_MIUI_ZRAM_MEMORY_TRACKING
+struct zram_pages_life {
+	unsigned int time_nr;
+	int *time_list;
+	unsigned long *lifes;
+	struct rcu_head rcu;
+};
+#endif
 
 struct zram {
 	struct zram_table_entry *table;
@@ -157,6 +171,12 @@ struct zram {
 #endif
 #ifdef CONFIG_ZRAM_MEMORY_TRACKING
 	struct dentry *debugfs_dir;
+#endif
+#ifdef CONFIG_MIUI_ZRAM_MEMORY_TRACKING
+	struct zram_pages_life __rcu *pages_life;
+	ktime_t first_time;
+	ktime_t last_time;
+	atomic64_t avg_size;
 #endif
 };
 
