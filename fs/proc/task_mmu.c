81cb1c50843cd9215ac2527f34a65b4ef0106ead
ZyCromerZ <neetroid97@gmail.com>
2021-09-29 23:34:19 +0700

    proc: mmu: use sultan pid by sultan_pid_mode
    * file should be arround : /sys/modules/proc/sultan_pid_mode
    * 0 = off
    * 1 = use sultan pid
    * 2 = use sultan pid shrink
    * 3 = use sultan pid and pid shrink
    
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -27,15 +27,6 @@
 #include <asm/tlbflush.h>
 #include "internal.h"
 
-bool sultan_pid = false;
-module_param(sultan_pid, bool, 0644);
-
-bool sultan_pid_shrink = false;
-module_param(sultan_pid_shrink, bool, 0644);
-
-bool use_sultan_pid = false;
-module_param(use_sultan_pid, bool, 0644);
-
 #define SEQ_PUT_DEC(str, val) \
 		seq_put_decimal_ull_width(m, str, (val) << (PAGE_SHIFT-10), 8)
 void task_mem(struct seq_file *m, struct mm_struct *mm)
@@ -89,6 +80,33 @@ void task_mem(struct seq_file *m, struct mm_struct *mm)
 	hugetlb_report_usage(m, mm);
 }
 #undef SEQ_PUT_DEC
+
+int sultan_pid_mode = 0;
+module_param(sultan_pid_mode, int, 0644);
+
+bool check_current_app(int mode)
+{
+	if (sultan_pid_mode <= 0)
+		sultan_pid_mode = 0;
+
+	if (sultan_pid_mode >= 4)
+		sultan_pid_mode = 3;
+
+	if (sultan_pid_mode == 0)
+		return false;
+
+	if (mode == 2) {
+		// sultan pid shrink
+		if (sultan_pid_mode == 1 || sultan_pid_mode == 3)
+			return true;
+	}
+	if (mode == 1) {
+		// sultan pid
+		if (sultan_pid_mode == 2 || sultan_pid_mode == 3)
+			return true;
+	}
+	return false;
+}
 
 unsigned long task_vsize(struct mm_struct *mm)
 {
@@ -532,12 +550,7 @@ static int show_vma_header_prefix(struct seq_file *m, unsigned long start,
 	/* Supports printing up to 40 bits per virtual address */
 	BUILD_BUG_ON(CONFIG_ARM64_VA_BITS > 40);
 
-	if (use_sultan_pid)
-	{
-		sultan_pid_shrink = true;
-	}
-
-	if (sultan_pid_shrink)
+	if (check_current_app(1))
 	{
 		/* 
 		 * shrinks the PID map output to be as small as
@@ -788,11 +801,7 @@ static const struct seq_operations proc_tid_maps_op = {
 
 static int pid_maps_open(struct inode *inode, struct file *file)
 {
-	if (use_sultan_pid)
-	{
-		sultan_pid = true;
-	}
-	if (sultan_pid)
+	if (check_current_app(2))
 		return do_maps_open(inode, file, &proc_pid_maps_op_sultanpid);
 	else return do_maps_open(inode, file, &proc_pid_maps_op);
 }
