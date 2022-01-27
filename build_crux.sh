126f5cbb7a548dc6b4478ce9b0c477e115b4b4b2
TheVoyager0777 <l72727746@outlook.com>
2022-01-27 14:59:27 +0800

    add build script for crux

---
+++ build_crux.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+yellow='\033[0;33m'
+white='\033[0m'
+red='\033[0;31m'
+gre='\e[0;32m'
+
+ANYKERNEL3_DIR=$PWD/AnyKernel3
+FINAL_KERNEL_ZIP=kernel-Tucana-r-VoyagerIII-$(git rev-parse --short=7 HEAD).zip
+IMAGE_GZ=$PWD/out/arch/arm64/boot/Image.gz
+ccache_=`which ccache`
+export ARCH=arm64
+export SUBARCH=arm64
+export HEADER_ARCH=arm64
+export CLANG_PATH=
+
+export KBUILD_BUILD_HOST="Voayger-sever"
+export KBUILD_BUILD_USER="TheVoyager"
+
+# make mrproper O=out || exit 1
+make raphael_defconfig O=out_raphael || exit 1
+
+Start=$(date +"%s")
+
+make -j$(nproc --all) \
+	O=out_raphael \
+	CC="${ccache_} ${CLANG_PATH}/bin/clang" \
+	CLANG_TRIPLE=/bin/aarch64-linux-gnu- \
+	CROSS_COMPILE=/bin/aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=/bin/arm-linux-gnueabi- || > build.log
+
+exit_code=$?
+End=$(date +"%s")
+Diff=$(($End - $Start))
+
+echo -e "$gre << Build completed in $(($Diff / 60)) minutes and $(($Diff % 60)) seconds >> \n $white"
