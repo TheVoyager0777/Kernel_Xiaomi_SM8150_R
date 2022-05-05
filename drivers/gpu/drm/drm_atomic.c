ed05c77df827207e393352a29ac759f50778ca86
John Galt <johngaltfirstrun@gmail.com>
2022-05-05 15:15:27 -0400

    drm_atomic: stop boosting on frame commits
    Since fixing uclamp prefer idle flag, this has been shown to be
    unnecessary.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -30,7 +30,6 @@
 #include <drm/drm_atomic.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
-#include <linux/devfreq_boost.h>
 #include <linux/pm_qos.h>
 #include <linux/sync_file.h>
 
@@ -2248,11 +2247,6 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	if ((arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
-
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
-
-	}
 
 	drm_modeset_acquire_init(&ctx, 0);
 
