c377c716a43bcc7925010d8cd12ae1bdfb6efc34
TheVoyager0777 <l72727746@outlook.com>
2022-06-05 00:14:34 +0800

    Revert "drm/msm/sde: Don't allocate memory dynamically for CRTC atomic check"
    This reverts commit f24e85ff6e73c055a4bb018444578540794b029f.

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -5574,6 +5574,7 @@ static int sde_crtc_atomic_check(struct drm_crtc *crtc,
 
 	memset(pstates, 0, sizeof(pstates));
 	memset(multirect_plane, 0, sizeof(multirect_plane));
+
 	mode = &state->adjusted_mode;
 	SDE_DEBUG("%s: check", sde_crtc->name);
 
