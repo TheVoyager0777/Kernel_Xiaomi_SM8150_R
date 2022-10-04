8ae6cfbe5e953485844e315d90219d8b17d305c9
TheVoyager0777 <l727127746@outlook.com>
2022-10-04 23:35:53 +0800

    sched/uclamp: Make hooks usable

--- kernel/sched/android.h
+++ kernel/sched/android.h
@@ -7,15 +7,45 @@
  * simpler to notice what is, and is not, an upstream change this way over time.
  */
 
+#include "sched.h"
 
-/*
- * task_may_not_preempt - check whether a task may not be preemptible soon
- */
-static inline bool task_may_not_preempt(struct task_struct *task, int cpu)
+#ifdef CONFIG_UCLAMP_TASK
+static inline bool uclamp_boosted(struct task_struct *p)
 {
-	return false;
+	return uclamp_eff_value(p, UCLAMP_MIN) > 0;
 }
 
+static inline bool uclamp_latency_sensitive(struct task_struct *p)
+{
+	struct cgroup_subsys_state *css = task_css(p, cpu_cgrp_id);
+	struct task_group *tg;
+
+	if (!css)
+		return false;
+	tg = container_of(css, struct task_group, css);
+
+	return tg->latency_sensitive;
+}
+
+/*
+ * Return whether the task on the given cpu is currently non-preemptible
+ * while handling a potentially long softint, or if the task is likely
+ * to block preemptions soon because it is a ksoftirq thread that is
+ * handling slow softints.
+ */
+static inline bool
+task_may_not_preempt(struct task_struct *task, int cpu)
+{
+	__u32 softirqs = per_cpu(active_softirqs, cpu) |
+			 __IRQ_STAT(cpu, __softirq_pending);
+	struct task_struct *cpu_ksoftirqd = per_cpu(ksoftirqd, cpu);
+
+	return ((softirqs & LONG_SOFTIRQ_MASK) &&
+		(task == cpu_ksoftirqd ||
+		 task_thread_info(task)->preempt_count & SOFTIRQ_MASK));
+}
+
+#else
 static inline bool uclamp_boosted(struct task_struct *p)
 {
 	return false;
@@ -25,3 +55,9 @@ static inline bool uclamp_latency_sensitive(struct task_struct *p)
 {
 	return false;
 }
+
+static inline bool task_may_not_preempt(struct task_struct *task, int cpu)
+{
+	return false;
+}
+#endif
