CapShift/Kernel_Xiaomi_sm8250_S

sched: Merge some upstream changes

kernel/sched/cpufreq_schedutil.c
@@ -592,6 +592,8 @@ static unsigned long sugov_iowait_apply(struct sugov_cpu *sg_cpu, u64 time,
 	 * into the same scale so we can compare.
 	 */
 	boost = (sg_cpu->iowait_boost * max) >> SCHED_CAPACITY_SHIFT;
+	boost = uclamp_rq_util_with(cpu_rq(sg_cpu->cpu), boost, NULL);
+
 	return max(boost, util);
 }
 

TheVoyager0777/Kernel_Xiaomi_SM8150_R

kernel/sched/cpufreq_schedutil.c
@@ -367,6 +367,7 @@ static void sugov_iowait_boost(struct sugov_cpu *sg_cpu, unsigned long *util,
 	boost_util = sg_cpu->iowait_boost;
 	boost_max = sg_cpu->iowait_boost_max;
 
+	boost_max = uclamp_rq_util_with(cpu_rq(sg_cpu->cpu), boost_max, NULL);
 	if (*util * boost_max < *max * boost_util) {
 		*util = boost_util;
 		*max = boost_max;